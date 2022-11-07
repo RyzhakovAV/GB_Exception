d3ab3baa260a20d950e27daf121c590d8ed86c29
RyzhakovAV <rjizhakov@mail.ru>
2022-11-07 10:14:15 +0500

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        int n = new Random().nextInt(100);
+        System.out.println(n);
+    }
+}
