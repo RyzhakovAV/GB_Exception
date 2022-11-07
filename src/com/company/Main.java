40e0426195092af412296d6d662631d4398825b8
RyzhakovAV <rjizhakov@mail.ru>
2022-11-07 10:55:09 +0500

    add task 1 from seminar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,28 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
+    public static final int min = 5;
+
+    public static int codeError(int[] arr, int count, int index) {
+        int sum = 0;
+        for (int digit : arr) {
+            sum += digit;
+        }
+        if (arr == null) return -3;
+        else if (arr.length < min) return -1;
+        else if (sum < 100 && arr[index] == 5) return -4;
+        else {
+            Arrays.sort(arr);
+            if (Arrays.binarySearch(arr, count) < 0) return -2;
+        }
+        return 0;
+    }
 
     public static void main(String[] args) throws Exception{
-        int n = new Random().nextInt(100);
-        System.out.println(n);
+        int[] array = {0, -4, 5, 3, 2, 1, 9, 56, 5, 3, 7};
+        System.out.println(codeError(array, 56, 2));
     }
 }
