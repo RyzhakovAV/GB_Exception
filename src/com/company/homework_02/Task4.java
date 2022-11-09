5bd8b0e5f86b438399080f5017cfc41676c7f429
RyzhakovAV <rjizhakov@mail.ru>
2022-11-09 22:45:53 +0500

    add task 3

---
+++ src/com/company/homework_02/Task4.java
@@ -0,0 +1,22 @@
+package com.company.homework_02;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+/*
+Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
+Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+ */
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите строку. Для выхода из программы введите exit");
+        String str = console.nextLine();
+        while (!str.equals("exit")) {
+            if (str.equals("")) throw new RuntimeException("Ошибка ввода, строка пустая");
+            System.out.println(str);
+            str = console.nextLine();
+        }
+    }
+}
